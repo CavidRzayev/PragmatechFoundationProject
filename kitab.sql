625f3a61931ffe3c47be89bae7fbfb728926f8c0
murad48oguz <ebulzade@inbox.ru>
2021-01-05 15:18:23 +0400

    changes'

---
+++ kitab.sql
@@ -0,0 +1,12 @@
+CREATE TABLE kitab(
+  kitabAdi varchar (30),
+  yazici varchar (30),
+  nesriyyatEvi (30),
+  sehifeSayi INT
+  PRIMARY KEY(kitabAdi)
+);
+
+INSERT INTO kitab (kitabAdi,yazici,nesriyyatEvi,sehifeSayi)
+VALUES ("Martin Eden","Cek London", "Xarici","300" );
+
+
---
+++ yazici.sql
@@ -0,0 +1,7 @@
+CREATE TABLE yazici(
+    adsoyad varchar (30),
+    elaqeMail varchar (30)
+);
+
+INSERT INTO yazici (adsoyad,elaqMail)
+VALUES ("Cingiz Abdullayev","cingizabdullayev@gmail.com" );
